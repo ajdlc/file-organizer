40a23abf148babd7d8d83a24fa78c7a5ab3b04f7
AJ De La Costa <ajdlc47@gmail.com>
2020-01-20 14:49:17 -0800

    Init commit

---
+++ app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const fs = require("fs").promises;
+
+const app = express();
+
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.get("/", async (req, res) => {
+    const file = await fs.readFile(__dirname + "/file_metadata/test.json");
+    res.send(JSON.parse(file.toString()));
+});
+
+app.get("/files", async (req, res) => {
+    const dirs = await fs.readdir(__dirname + "/file_metadata");
+    console.log(dirs);
+
+    res.send(dirs);
+})
+
+app.get("/homefolder", async (req, res) => {
+    const homeFolder = await fs.readdir(__dirname + "../../..");
+    
+    res.send(homeFolder);
+    
+})
+
+app.post("/folders", async (req, res) => {
+    const name = req.body.name;
+
+    try {
+        await fs.mkdir(__dirname + "/" + name);
+        res.send({
+            msg: "Directory made successfully!",
+            loction: __dirname + "/" + name
+        });
+    } catch (e) {
+        if (e.errno === -4075) {
+            res.status(400).send({
+                msg: "THe directory already exists!",
+                e
+            })
+        }
+        res.status(400).send({msg: "Something went wrong and there was an error.", e});
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Listing on ${port}!`);
+    
+})
